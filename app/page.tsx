f9740a7639381d1c76c2c4739449eaf5dc6815e7
unknown <chatzteam@gmail.com>
2025-07-20 18:36:19 +0000

    removed access to the builder

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { LessonBuilder } from "@/components/lesson-builder"
+import { LessonViewer } from '@/components/viewer/LessonViewerUpload';
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <LessonBuilder />
+      <LessonViewer />
     </main>
   )
 }
